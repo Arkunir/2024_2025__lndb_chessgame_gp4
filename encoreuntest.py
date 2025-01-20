18a230a8f4c39bbeaa6e56ada238c7ff315da1e8
romanbrnv <romanbrnv34.com@gmail.com>
2025-01-20 11:49:52 +0100

    commit, qui permettra à mes collegue d'avancer sur la tache noble qui nous a été confié par monsieur pioche

---
+++ encoreuntest.py
@@ -0,0 +1,8 @@
+# Nouveau fichier encoreuntest.py sans "root"
+
+# Ajoutez ici le code nécessaire sans utiliser "root"
+def main():
+    print("Ceci est un exemple de code sans 'root'.")
+
+if __name__ == "__main__":
+    main()
